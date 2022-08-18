aadc7a2268f59860b68b06050c05da38f4b08be1
mostafamt <mostafamt60@yahoo.com>
2022-08-18 19:20:35 +0200

    fix search again

--- pages/events/search.js
+++ pages/events/search.js
@@ -14,7 +14,7 @@ export default function SearchPage({ events }) {
       {events.length === 0 && <h3>No events to show</h3>}
 
       {events.map((evt) => (
-        <EventItem key={evt.id} evt={evt.attributes} />
+        <EventItem key={evt.id} evt={evt} />
       ))}
     </Layout>
   );
